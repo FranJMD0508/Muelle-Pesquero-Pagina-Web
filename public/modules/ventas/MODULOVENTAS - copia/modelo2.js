82bd0bd756dd4dd79c06faae9a4193cb91f2dd47
andre <183250668+aarevalo07@users.noreply.github.com>
2024-12-02 01:19:21 -0400

    Se arreglo errores con el css y se creo otro css para no descontrolar su css Tambien se cambio la imagen de lupa por icono de lupa Se creo un archivo css que es un respaldo a lo que se hará mas adelante Se creo un archivo js para que relacione la api y vaya generando uno a uno los productos del almacen y si no hay la cantidad se indica que no esta disponible

---
+++ public/modules/ventas/MODULOVENTAS - copia/modelo2.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const url = "";
+    fetch(url+"/API/pescados/", {
+        method: "GET",
+        headers: new Headers({
+            "ngrok-skip-browser-warning": "69420", // Para saltar la advertencia de Ngrok
+        }),
+    })
+    .then((response) => response.json())
+    .then((datos) => {
+        const menu = document.querySelector(".menu");
+        datos.data.map((obj) => {
+            const articulo = document.createElement("article").classList.add("item");
+            const contenido = crearArticle(obj); 
+            if (obj.cantidad===0) {
+                articulo.innerHTML = `
+                <div id="${obj.name}" style="height:100%; width: 100%; z-index: 1; background-color: rgb(204,204,204,0.3)"><h1>No Disponible</h1></div>
+                ${contenido}
+                `;
+            } else {
+                articulo.innerHTML = `
+                <div style="display:none; height:100%; width: 100%; z-index: 1; background-color: rgb(204,204,204,0.3)"><h1>No Disponible</h1></div>
+                ${contenido}
+                `;
+            }
+            menu.appendChild(articulo);
+        })
+    }).catch(error => console.error("Error al obtener los objetos: ", error));
+
+    function crearArticle(obj) {
+        const contenido = `
+        <h2>${obj.name}</h2>
+        <img src="${obj.imagen+".jpg"}" alt="${obj.name} data-hover="${obj.name+"2.jpg"}">
+        <p>${obj.price}</p>
+        <p>${obj.description}</p>
+        <button class="order-button">ORDENAR</button>
+        `;
+        return contenido;
+    }
+
+    const items = document.querySelectorAll(".item img");
+
+    items.forEach(function(item) {
+        const originalSrc = item.src;
+        const hoverSrc = item.getAttribute("data-hover");
+
+        item.addEventListener("mouseenter", function() {
+            item.src = hoverSrc;
+        });
+
+        item.addEventListener("mouseleave", function() {
+            item.src = originalSrc;
+        });
+    });
+});
