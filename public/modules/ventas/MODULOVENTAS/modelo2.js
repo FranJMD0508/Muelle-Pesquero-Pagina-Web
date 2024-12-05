e9ce491a188b50e12d2b6952a0e7d612c7977967
andre <183250668+aarevalo07@users.noreply.github.com>
2024-12-05 01:57:43 -0400

    Se anadio funcion de leo y andre (solicitudes.js) falta enlazar su funcionalidad a la api, en cuanto a la pagina de ventas falta arreglar diseños del canvas del carrito y mostrar en el carrito los objetos nuevos (no tengo IA que me ayude) y falta generar la factura de compras al comprar en el carrito (David gonzales tiene esafuncion). FIN BESOS

--- public/modules/ventas/MODULOVENTAS/modelo2.js
+++ public/modules/ventas/MODULOVENTAS/modelo2.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const url = "https://f786-190-120-250-84.ngrok-free.app";
-    fetch(url+"/inventario/pescado/", {
+    const url = "https://8b95-190-120-250-84.ngrok-free.app";
+    fetch(url+"/api/pescados/", {
         method: "GET",
         headers: new Headers({
             "ngrok-skip-browser-warning": "69420", // Para saltar la advertencia de Ngrok
@@ -8,31 +8,34 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then((response) => response.json())
     .then((datos) => {
-        const menu = document.querySelector("#Fileteados");
+        const menu = document.querySelector(".menu");
         datos.data.map((obj) => {
-            const articulo = document.createElement("article").classList.add("item col-sm-6 col-md-4 col-lg-3 mb-4");
+            const articulo = document.createElement("article");
+            articulo.classList.add("item", "col-sm-6", "col-md-4", "col-lg-3", "mb-4");
+            articulo.setAttribute("data-product", obj.nombre.toLowerCase());
+
             const contenido = crearArticle(obj); 
             if (obj.cantidad===0) {
                 articulo.innerHTML = `
-                <div id="${obj.name}" style="position: relative; height:100%; width: 100%; z-index: 1; background-color: rgb(204,204,204,0.3)"><h1>No Disponible</h1></div>
-                ${contenido}
-                `;
+                    ${contenido} 
+                    <button class="order-button" disabled="true" style=".order-button:hover {background-color: #fff !important;color: #fe9 !important;}">NO DISPONIBLE</button>
+                    `;
             } else {
                 articulo.innerHTML = `
-                <div style="display:none; height:100%; width: 100%; z-index: 1; background-color: rgb(204,204,204,0.3)"><h1>No Disponible</h1></div>
-                ${contenido}
-                `;
+                    ${contenido} 
+                    <button class="order-button" >ORDENAR</button>
+                    `;
             }
             menu.appendChild(articulo);
         })
     }).catch(error => console.error("Error al obtener los objetos: ", error));
 
     function crearArticle(obj) {
+        const nombre = obj.nombre.toLowerCase();
         const contenido = `
-        <h2>${obj.name}</h2>
-        <img src="${obj.imagen+".jpg"}" alt="${obj.name} data-hover="${obj.name+"2.jpg"}">
-        <p>${obj.price}</p>
-        <button class="order-button">ORDENAR</button>
+        <img src="${nombre+".jpg"}" alt="${nombre} data-hover="${nombre+"2.jpg"}">
+        <h2>${obj.nombre}</h2>
+        <p id="${nombre}" value="${obj.precio}">Precio: $${obj.precio}/kg</p>
         `;
         return contenido;
     }
@@ -81,14 +84,14 @@ document.addEventListener("DOMContentLoaded", function() {
     let carrito = [];
     const ordenarbtn = document.querySelectorAll(".order-button");
 
-    // fetch(url+"/inventario/pescado/", {
-    //     method: "GET",
-    //     headers: new Headers({
-    //         "ngrok-skip-browser-warning": "69420", // Para saltar la advertencia de Ngrok
-    //     }),
-    // })
-    // .then((response) => response.json())
-    // .then((datos) => {
+    fetch(url+"/api/inventario/pescado/", {
+        method: "GET",
+        headers: new Headers({
+            "ngrok-skip-browser-warning": "69420", // Para saltar la advertencia de Ngrok
+        }),
+    })
+    .then((response) => response.json())
+    .then((datos) => {
         ordenarbtn.forEach(button => {
             button.addEventListener('click', function(event) {
                 // Obtiene el artículo correspondiente al botón que fue clickeado
@@ -97,35 +100,34 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Obtiene el nombre del producto
                 const nombreProducto = articulo.querySelector('h2').textContent;
                 console.log(nombreProducto);
+                const precio = articulo.querySelector('p').value;
         
                 // TENGO QUE HACER LA VERIFICACION, MAÑANA PIENSO
-                const productoExistente = carrito.find(item => item.producto === nombreProducto);
+                
                 
                 // ESTE IF ESTÁ MAAAAAAAAL ESTOY VALIDANDO SI EXISTE
-                if (productoExistente) {
-                    productoExistente.cantidad++;
+                if (carrito.find(item => item.producto === nombreProducto)) {
+                    return;
                 } else {
                     carrito.push({
                         producto: nombreProducto,
                         cantidad: 1,
-                        precio: nose
+                        precio: precio
                     });
+                    actualizarListaCarrito();
                 }
         
                 // Actualiza la lista del carrito
-                actualizarListaCarrito();
             });
         });
-    // }).catch(error => console.error("Error al obtener los objetos: ", error));
+    }).catch(error => console.error("Error al obtener los objetos: ", error));
 
     function actualizarListaCarrito() {
-        // Limpia la lista actual
-        listaCarrito.innerHTML = '';
     
         // Itera sobre el carrito y agrega cada producto a la lista
         carrito.forEach(item => {
             const li = document.createElement('li');
-            li.textContent = `${item.producto}: ${item.cantidad}kg`; //VERÉ SI LO INCREMENTO O LE PONGO UN INPUT
+            li.innerHTML = `<span>${item.producto}</span> <input value="${item.cantidad}" type="number"> <i id="incremento">+</i> <i id="decremento">-</i> <i id="eliminar">borrar</i>`
             listaCarrito.appendChild(li);
         });
     }
